ad7e0aaa538ff868d83f82fb7890ad7a0318af83
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-28 14:37:39 -0800

    Rendering all players

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -4,12 +4,22 @@ import Player from "./Player";
 function PlayerList(props) {
 
 
-  const onePlayer = props.playerData[0];
+  const players = props.playerData.map((player) => {
+    return (
+      <Player
+        key={player.gamerTag}
+        gamerTag={player.gamerTag}
+        firstName={player.firstName}
+        lastName={player.lastName}
+        wins={player.wins}
+      />
+    );
+  });
 
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
+      {players}
     </section>
   );
 }
