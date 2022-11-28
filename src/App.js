da223c0afa2250ba3aec42e37e3262e284bd5eba
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-28 12:26:05 -0800

    Create mock data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import PlayerList from './components/PlayerList';
+import MatchList from './components/MatchList';
 
 function App() {
   return (
     <div className="App">
+      <h1>
+        Tourney Matches <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
