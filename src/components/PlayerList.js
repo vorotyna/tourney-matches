cd788f16cf75183247dac2115d99a9eb0f594d50
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-28 11:52:42 -0800

    Create PlayerList component

---
+++ src/components/PlayerList.js
@@ -0,0 +1,12 @@
+import Player from "./Player";
+
+function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      <Player />
+    </section>
+  );
+}
+
+export default PlayerList;
