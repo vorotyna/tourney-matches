090db50aec0f25b9f933859528f1929d14ce4bb1
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-28 13:53:25 -0800

    Conditional rendering

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,12 +4,9 @@ function Player(props) {
       <h1>
         {props.firstName} <span>{props.gamerTag}</span> {props.lastName}
       </h1>
-      {/* To be shown when there is no wins */}
-      <h2 className="zero">Currently with no wins :(</h2>
-      {/* To be shown when there is 1 win */}
-      <h2>Currently at 1 win</h2>
-      {/* To be shown when there is more than one win */}
-      <h2>Currently at {props.wins} wins</h2>
+      {props.wins === 0 && <h2 className="zero">Currently with no wins :(</h2>}
+      {props.wins === 1 && <h2>Currently at 1 win</h2>}
+      {props.wins > 1 && <h2>Currently at {props.wins} wins</h2>}
     </article>
   );
 }
