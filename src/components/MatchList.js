de66f6dc6cfef0c6b415c52c76eb537cc74e0746
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-28 11:57:35 -0800

    Create MatchList component

---
+++ src/components/MatchList.js
@@ -0,0 +1,12 @@
+import Match from "./Match";
+
+function MatchList() {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      <Match />
+    </section>
+  );
+}
+
+export default MatchList;
